60cf3ba5e3d984328fef1a695474ee45d27ac546
CodeBlankCjj <cj13480394274@163.com>
2021-06-05 16:11:27 +0800

    deploy

---
+++ precache-manifest.ce07259e6dab97af48af3d34dfb299d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be48080a75a2e59f5360",
+    "url": "/morney-Vue-website/css/app.556eb206.css"
+  },
+  {
+    "revision": "0c9ef325fb360570fa4dcfc1e7a9f0a1",
+    "url": "/morney-Vue-website/img/logo.png"
+  },
+  {
+    "revision": "307bfa04bf79952d5f45a8198310d2b3",
+    "url": "/morney-Vue-website/index.html"
+  },
+  {
+    "revision": "be48080a75a2e59f5360",
+    "url": "/morney-Vue-website/js/app.55f1fe00.js"
+  },
+  {
+    "revision": "d46d47f6d5326c0bc6ba",
+    "url": "/morney-Vue-website/js/chunk-vendors.fd647dae.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-Vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-Vue-website/robots.txt"
+  }
+]);
